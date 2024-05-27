d91813a09c8ece9a6a807a8f753bfea92e396503
SophieSued <163852514+SophieSued@users.noreply.github.com>
2024-05-27 09:10:42 -0300

    Contraseña tp

---
+++ Assets/LoginManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LoginManager : MonoBehaviour
+{
+    public InputField inputpassword;
+    public string password = "Sophie";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CheckPassword()
+    {
+        if (inputpassword.text == password)
+        {
+            Debug.Log("Tiene acceso");
+        }
+       
+        else
+        {
+            Debug.Log("Acceso denegado");
+        }
+    }
+}
